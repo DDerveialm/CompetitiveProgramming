26e9745e19c2b0c7893111076187ecfe43ca422f
XDevil <andy890306@gmail.com>
2020-08-22 15:20:47 +0800

    Codeforces_Round#665

--- codeforces/global_round_10/G.cpp
+++ codeforces/global_round_10/G.cpp
@@ -5,12 +5,10 @@
 #include <stack>
 #include <queue>
 #include <algorithm>
-#include <limits>
-#include <bitset>
 
 using namespace std;
 
-int mask(string s) {
+inline int mask(const string& s) {
 	int m = 0;
 	for (int i = 0; i < s.size(); ++i)
 		if (s[i] == '1')
@@ -25,48 +23,40 @@ int main() {
 	
 	int n, m, k;
 	cin >> n >> m >> k;
-	string origin, ideal;
-	cin >> origin >> ideal;
-	vector<int> p(k);
+	string S, T;
+	cin >> S >> T;
+	vector<int> dpS(1 << k, 1e9), dpT(1 << k, -1e9);
+	dpS[mask(S)] = 0;
+	dpT[mask(T)] = 0;
+	vector<int> p(k); // record the i-th bit in begining is now at p[i]
 	for (int i = 0; i < k; ++i) p[i] = i;
-	vector<vector<int>> dp(2, vector<int> (1 << k, 0));
-	for (int i = 0; i < (1 << k); ++i) {
-		dp[0][i] = 1e9;
-		dp[1][i] = -1e9;
-	}
-	dp[0][mask(origin)] = 0;
-	dp[1][mask(ideal)] = 0;
 	for (int i = 1; i <= n; ++i) {
 		int a, b;
 		cin >> a >> b;
 		--a; --b;
+		swap(S[p[a]], S[p[b]]);
+		swap(T[p[a]], T[p[b]]);
 		swap(p[a], p[b]);
-		string x(k, '0'), y(k, '0');
-		for (int j = 0; j < k; ++j) {
-			x[p[j]] = origin[j];
-			y[p[j]] = ideal[j];
-		}
-		dp[0][mask(x)] = min(i, dp[0][mask(x)]);
-		dp[1][mask(y)] = i;
+		dpS[mask(S)] = min(i, dpS[mask(S)]);
+		dpT[mask(T)] = i;
 	}
 
-	int c1 = count(origin.begin(), origin.end(), '1'), c2 = count(ideal.begin(), ideal.end(), '1');
+	int c1 = count(S.begin(), S.end(), '1'), c2 = count(T.begin(), T.end(), '1');
 	int Max = -1;
 	pair<int, int> ans;
 	for (int i = (1 << k) - 1; i >= 0; --i) {
-		if (dp[1][i] - dp[0][i] >= m) {
-			int cb = k - (c1 + c2 - 2 * __builtin_popcount(i));
+		if (dpT[i] - dpS[i] >= m) {
+			int cb = k - c1 - c2 + 2 * __builtin_popcount(i);
 			if (cb > Max) {
 				Max = cb;
-				ans = {dp[0][i] + 1, dp[1][i]};
+				ans = {dpS[i] + 1, dpT[i]};
 			}
 		}
-		for (int j = 0; j < k; ++j) {
+		for (int j = 0; j < k; ++j)
 			if (i & (1 << j)) {
-				dp[0][i ^ (1 << j)] = min(dp[0][i ^ (1 << j)], dp[0][i]);
-				dp[1][i ^ (1 << j)] = max(dp[1][i ^ (1 << j)], dp[1][i]);
+				dpS[i ^ (1 << j)] = min(dpS[i ^ (1 << j)], dpS[i]);
+				dpT[i ^ (1 << j)] = max(dpT[i ^ (1 << j)], dpT[i]);
 			}
-		}
 	}
 	cout << Max << endl;
 	cout << ans.first << " " << ans.second << endl;
