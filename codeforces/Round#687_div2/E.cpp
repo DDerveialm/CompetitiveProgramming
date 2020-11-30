0d149684844f194ee094c509f33639744e99c0bd
XDevil <andy890306@gmail.com>
2020-11-30 22:08:57 +0800

    Codeforces_Educational_Round_99

--- codeforces/Round#687_div2/E.cpp
+++ codeforces/Round#687_div2/E.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -20,13 +21,34 @@ void solve() {
 	cout << ans << '\n';
 }
 
+void genius_solve() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(n);
+	for (auto& x : v) cin >> x;
+	sort(v.rbegin(), v.rend());
+
+	priority_queue<long long> pq{};
+	for (int i{0}; i < k + 1; ++i) pq.push(0);
+	
+	long long ans{0};
+	for (int i{0}; i < n; ++i) {
+		auto t{pq.top()};
+		pq.pop();
+		ans += t;
+		pq.push(t + v[i]);
+	}
+
+	cout << ans << '\n';
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
 	int t{1};
 	// cin >> t;
-	while (t--) solve();
+	while (t--) genius_solve();
 
 	return 0;
 }
