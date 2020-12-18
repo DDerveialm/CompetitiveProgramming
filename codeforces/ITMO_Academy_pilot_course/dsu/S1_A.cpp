ae02f9a5a44682695e8f0e123b913a422ff6923a
XDevil <andy890306@gmail.com>
2020-12-18 15:55:52 +0800

    Codeforces_Educational_Round_100

--- codeforces/ITMO_Academy_pilot_course/dsu/S1_A.cpp
+++ codeforces/ITMO_Academy_pilot_course/dsu/S1_A.cpp
@@ -4,43 +4,22 @@
 
 using namespace std;
 
-class DS { //disjoint set
+// fast disjoint set union
+class DSU {
+	vector<int> p, sz;
 public:
-	explicit DS(int);
-	int getID(int);
-	int unionn(int, int);
-	void deBug() {
-		for (auto& x : set) cout << x << " ";
-		cout << endl;
-		for (auto& x : sz) cout << x << " ";
-		cout << endl;
+	explicit DSU(int n) : p(n, -1), sz(n, 1) {}
+	int find(int x) { // path compression
+		if (p[x] == -1) return x;
+		return p[x] = find(p[x]);
 	}
-private:
-	vector<int> set, sz;
-};
-
-DS::DS(int n) : set(n), sz(n) {
-	for (int i{0}; i < n; ++i)
-		set[i] = i, sz[i] = 1;
-}
-
-int DS::getID(int x) {
-	if (x == set[x]) return x;
-
-	auto r{getID(set[x])};
-	set[x] = r;
-	return r;
-}
-
-int DS::unionn(int x, int y) {
-	auto rx{getID(x)}, ry{getID(y)};
-	if (rx != ry) {
+	void unionn(int x, int y) { // union by size
+		auto rx{find(x)}, ry{find(y)};
+		if (rx == ry) return ;
 		if (sz[rx] < sz[ry]) swap(rx, ry);
-		set[ry] = rx;
-		sz[rx] += sz[ry], sz[ry] = 0;
+		p[ry] = rx, sz[rx] += sz[ry];
 	}
-	return rx;
-}
+};
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -48,16 +27,17 @@ int main() {
 
 	int n, m;
 	cin >> n >> m;
-	DS ds{n};
+	DSU dsu{n};
 	while (m--) {
 		string op{};
 		int u, v;
 		cin >> op >> u >> v, --u, --v;
-		if (op == "union"s)
-			ds.unionn(u, v);
-		else if (op == "get"s)
-			if (ds.getID(u) == ds.getID(v)) cout << "YES" << endl;
+		if (op == "union"s) {
+			dsu.unionn(u, v);
+		} else if (op == "get"s) {
+			if (dsu.find(u) == dsu.find(v)) cout << "YES" << endl;
 			else cout << "NO" << endl;
+		}
 	}
 
 	return 0;
