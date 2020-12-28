f2f22b9eb3770ec9dcfbedb822e80abefbcd27cd
XDevil <andy890306@gmail.com>
2020-12-29 00:31:01 +0800

    Codeforces_Educational_Round_101

--- codeforces/Round#691_div2/D.cpp
+++ codeforces/Round#691_div2/D.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <set>
 
 using namespace std;
 
@@ -11,21 +10,21 @@ void solve() {
 	int A{0}, B{0};
 	for (int i{1}; i <= n; ++i) cin >> a[i] >> b[i], A += a[i], B += b[i];
 
-	vector<vector<int>> dp0(n + 1, vector<int>(A + 1, 0));
-	vector<vector<int>> dp1(n + 1, vector<int>(A + 1, 0));
-	vector<set<int>> r(n + 1);
-	r[0].insert(0);
+	vector<vector<int>> dp0(n + 1, vector<int>(A + 1, -B));
+	vector<vector<int>> dp1(n + 1, vector<int>(A + 1, -B));
 	for (int i{1}; i <= n; ++i) {
-		for (int k{i}; k >= 1; --k) {
-			for (auto& t : r[k]) dp1[k][t] = dp0[k][t];
-			for (auto& t : r[k - 1]) dp1[k][t + a[i]] = max(dp1[k][t + a[i]], dp0[k - 1][t] + b[i]), r[k].insert(t + a[i]);
-		}
+		dp0[0][0] = 0;
+		for (int k{1}; k <= i; ++k)
+			for (int t{0}; t <= A; ++t) {
+				dp1[k][t] = dp0[k][t];
+				if (t >= a[i]) dp1[k][t] = max(dp1[k][t], dp0[k - 1][t - a[i]] + b[i]);
+			}
 		swap(dp0, dp1);
 	}
 
 	for (int k{1}; k <= n; ++k) {
 		double ans{0};
-		for (auto& t : r[k])
+		for (int t{0}; t <= A; ++t)
 			ans = max(ans, min(static_cast<double>(t), dp0[k][t] / 2.d + B / 2.d));
 		cout << ans << ' ';
 	}
